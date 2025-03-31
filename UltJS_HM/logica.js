6b1d16d2dd7a5fd4d76536b47bd4438469ec0675
dd <rldiego9999@gmail.com>
2025-03-31 22:53:43 +0100

    ggg

--- UltJS_HM/logica.js
+++ UltJS_HM/logica.js
@@ -29,3 +29,57 @@ console.log(acceso);
 edad = 16;
 acceso = edad > 17 ? "Ingreso permitido" : "No se puede ingresar";
 console.log(acceso);
+
+
+//el while
+
+let nombre = "Alberto";
+if (nombre != "Alberto")
+{
+	console.log("klkkkk");
+}
+else
+{
+	console.log("kalikoelectro");
+}
+
+let age = 0;
+while (age < 10)
+{
+	console.log(age);
+	age++;
+}
+
+// el doWhile
+
+let num = 0;
+do
+{
+	if (num % 2 == 0)
+	{
+		console.log("es par --> " + num);
+	}
+	num++;//recordar siempre el incrementador
+} while (num < 10);
+
+// el For 
+// usas la estructura -> (iniciador (puedes var); condicion; incremento)
+
+let frutaFrutita = ["manzana", "pera", "fresita", "banano"];
+for (let i = 0; i < frutaFrutita.length; i++)
+{
+	console.log(frutaFrutita[i]);
+}
+
+//el FOR OF
+
+let animales = ["Hola", "Mundo", "Bicho"];
+for(let animal of animales)//podemos crear una nueva var dentro de la anterior
+{
+	console.log(animal);
+}
+
+while (i < animales.length)//cuando usamos lenght busca entre numeros
+{
+	
+}
